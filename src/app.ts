612b6f6fa20f738fceb17f4ce1e5dafc6e36dce3
Stefano <stefanop21@outlook.es>
2024-06-04 18:01:13 -0500

    new: yargs plugin

--- src/app.ts
+++ src/app.ts
@@ -1,35 +1,5 @@
-const base: number = 5;
-const message: string = `
-=======================
-      Table of ${base}
-=======================\n`;
-let output: string = '';
+import { argv } from './config/plugins/yargs.plugin';
 
-const tableOfMultiplication = (number: number) => {
-  for (let i = 1; i <= 10; i++) {
-    output += `${number} x ${i} = ${number * i}\n`;
-  }
-};
+console.log(process.argv);
 
-tableOfMultiplication(base);
-
-output = message + output;
-console.log(output);
-
-//* Write the output to a file
-import fs from 'fs';
-
-const path = 'outputs';
-
-const createFile = (base: number, output: string) => {
-  fs.mkdir(path, { recursive: true }, (err: any) => {
-    if (err) throw new Error(err);
-  });
-
-  fs.writeFile(`${path}/table-${base}.txt`, output, (err: any) => {
-    if (err) throw new Error(err);
-    console.log(`table-${base}.txt created!`);
-  });
-};
-
-createFile(base, output);
+console.log(argv.b);
