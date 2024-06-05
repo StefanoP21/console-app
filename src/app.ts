c6ac568cb233656e93f9f9d82eb6492ebd841cb0
Stefano <stefanop21@outlook.es>
2024-06-04 21:35:35 -0500

    refactor: optimizar el código

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
+import { ServerApp } from './presentation/server-app';
 import { argv } from './config/plugins/yargs.plugin';
-
-// console.log(argv.b);
 
 (async () => {
   await main();
 })();
 
 async function main() {
-  console.log(argv);
+  const { b: base, l: limit, s: showTable } = argv;
+
+  ServerApp.run({ base, limit, showTable });
 }
