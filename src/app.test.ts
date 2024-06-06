23a5d125ec168ec4f8a99c1bb3c7ba42d026a242
Stefano <stefanop21@outlook.es>
2024-06-05 23:45:51 -0500

    test: main - App

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,5 +1,32 @@
-describe('App.ts', () => {
-  test('should first', () => {
-    expect(1).toBe(1);
+import { ServerApp } from './presentation/server-app';
+
+describe('mainApp', () => {
+  test('should call Server.run with argv values', async () => {
+    const serverRunMock = jest.fn();
+    ServerApp.run = serverRunMock;
+
+    process.argv = [
+      'node',
+      'app.ts',
+      '-b',
+      '5',
+      '-l',
+      '10',
+      '-s',
+      '-n',
+      'test-file',
+      '-d',
+      'test-destination',
+    ];
+
+    await import('./app');
+
+    expect(serverRunMock).toHaveBeenCalledWith({
+      base: 5,
+      limit: 10,
+      showTable: true,
+      fileName: 'test-file',
+      fileDestination: 'test-destination',
+    });
   });
 });
